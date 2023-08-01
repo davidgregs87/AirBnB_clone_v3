52c35fc8160f4ba23269f8918ba37c1872741f57
davidgregs87 <udohdavid27@gmail.com>
2023-08-01 19:41:43 +0100

    fixed error

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,4 +24,4 @@ def not_found(error):
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
     port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,8 +6,6 @@ from api.v1.views import app_views
 from models.state import State
 from models import storage
 
-classes = {"State": State}
-
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
 def view_states():
